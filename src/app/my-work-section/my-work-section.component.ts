d6d8a1b44ec222f2bc7f595c946df677394af8e8
Tuncay Dagdelen <sramos083@gmail.com>
2022-08-31 15:02:59 +0200

    toggle hover at my work

--- src/app/my-work-section/my-work-section.component.ts
+++ src/app/my-work-section/my-work-section.component.ts
@@ -9,22 +9,36 @@ export class MyWorkSectionComponent implements OnInit {
   constructor() { }
   showAngular = true;
   showJavaScript = true;
+  toggleAngular = false;
+  toggleJavaScript = false;
+  toggleAll = true;
   ngOnInit(): void {
   }
 
   showAllFunc() {
     this.showAngular = true;
     this.showJavaScript = true;
+    this.toggleAngular = false;
+    this.toggleJavaScript = false;
+    this.toggleAll = true;
   }
 
   showAngularFunc() {
     this.showJavaScript = false;
     this.showAngular = true;
+    this.toggleAngular = true;
+    this.toggleJavaScript = false;
+    this.toggleAll = false;
+
   }
 
   showJavaScriptFunc() {
     this.showAngular = false;
     this.showJavaScript = true;
+    this.toggleJavaScript = true;
+    this.toggleAll = false;
+    this.toggleAngular = false;
+
   }
 
 }
