74c654987e1c97981beb867f17a5291fc17777cf
Tuncay Dagdelen <sramos083@gmail.com>
2022-08-31 14:11:26 +0200

    my work filter implemented

--- src/app/my-work-section/my-work-section.component.ts
+++ src/app/my-work-section/my-work-section.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-work-section.component.scss']
 })
 export class MyWorkSectionComponent implements OnInit {
-
   constructor() { }
-
+  showAngular = true;
+  showJavaScript = true;
   ngOnInit(): void {
+  }
+
+  showAllFunc() {
+    this.showAngular = true;
+    this.showJavaScript = true;
+  }
+
+  showAngularFunc() {
+    this.showJavaScript = false;
+    this.showAngular = true;
+  }
+
+  showJavaScriptFunc() {
+    this.showAngular = false;
+    this.showJavaScript = true;
   }
 
 }
